f974734d2760b94298dba98c864121a0e0ad6b5c
Nobuyuki OSAWA <n-osawa@mapion.co.jp>
2014-02-20 15:47:39 +0900

    watch追加

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,26 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-	connect: {
-	  server: {
-	    options: {
-		  port: 9001,
-		  keepalive: true,
-		  hostname: 'localhost'
-		}
-	  }
-	}
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          keepalive: true,
+          hostname: 'localhost'
+        }
+      }
+    },
+    watch: {
+      html: {
+        files: ['**/*.html', '**/*.js', '**/*.css'],
+        options: {
+          livereload: true
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.registerTask('default', ['connect']);
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['connect', 'watch']);
 }
